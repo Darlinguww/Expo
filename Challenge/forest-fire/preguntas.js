a13f921ad6f7de4f06f573b3aee005dd97c6b5e6
Darlinguww <117539666+Darlinguww@users.noreply.github.com>
2023-10-25 17:01:03 -0500

    Add files via upload

---
+++ Challenge/forest-fire/preguntas.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "¿Qué tipo de energía se libera durante la combustión en incendios forestales desde una perspectiva termodinámica?",
+    answer: "Energía química",
+    options: [
+      "Energía cinética",
+      "Energía hidroeléctrica",
+      "Energía química ",
+      "Energía eólica"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Qué proceso termodinámico implica la expansión y compresión del aire debido a las corrientes ascendentes y descendentes de calor en la atmósfera?",
+    answer: "Ciclo atmosférico",
+    options: [
+      "Ciclo de Rankine ",
+      "Ciclo de Carnot",
+      "Ciclo de Stirling",
+      "Ciclo atmosférico"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Cuál de los siguientes no es un método utilizado en la supresión de incendios forestales?",
+    answer: "Inyección de oxígeno",
+    options: [
+      "Cortafuegos",
+      "Aspersores de agua ",
+      "Inyección de oxígeno ",
+      "Tala de árboles"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Qué propiedad física de un material se relaciona con su capacidad para conducir el calor?",
+    answer: "Conductividad térmica",
+    options: [
+      "Densidad",
+      "Conductividad térmica",
+      "Viscosidad",
+      "Viscosidad"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Cuál es uno de los procesos principales a través del cual el calor se propaga en un incendio forestal cuando se transfiere a través de materiales sólidos como troncos de árboles y hojas secas?",
+    answer: "Conducción",
+    options: [
+      "Conducción",
+      "Convección",
+      "Radiación",
+      "Evaporación"
+    ]
+  },
+  {
+    numb: 6,
+    question: "¿Cómo se llama el proceso de transferencia de calor que involucra el movimiento de un fluido, como el aire caliente, en un incendio forestal?",
+    answer: "Convección",
+    options: [
+      "Conducción",
+      "Convección",
+      "Radiación",
+      "Evaporación"
+    ]
+  },
+  {
+    numb: 7,
+    question: "¿Qué tipo de transferencia de calor en un incendio forestal implica la propagación del calor en forma de ondas electromagnéticas, como la luz y el calor infrarrojo?",
+    answer: "Radiación",
+    options: [
+      "Conducción",
+      "Convección",
+      "Radiación",
+      "Conducción de fluidos"
+    ]
+  },
+  {
+    numb: 8,
+    question: "¿Por qué el aire caliente se eleva en un incendio forestal?",
+    answer: "Debido a su menor densidad",
+    options: [
+      "Debido a la expansión del oxígeno",
+      "Debido a la congelación del aire",
+      "Debido a su mayor densidad",
+      "Debido a su menor densidad"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Desde una perspectiva termodinámica, ¿Por qué el mecanismo de los aspersores de agua para combatir los incendios forestales consiste en rociar finas gotas de agua durante los incendios?",
+    answer: "Aumentar la velocidad de reacción del fuego",
+    options: [
+      "Aumentar la temperatura ambiente",
+      "Añadir más oxígeno al fuego",
+      "Para que la evaporación sea más rápida",
+      "Aumentar la velocidad de reacción del fuego"
+    ]
+  },
+  {
+    numb: 10,
+    question: "¿Cómo se reduce la temperatura del área en los incendios forestales?",
+    answer: "A través de la convención, al consumir calor latente, provoca un descenso apreciable de la temperatura en la zona afectada.",
+    options: [
+      "A través de la evaporación se consume calor sensible lo cual disminuye la temperatura.",
+      "A través de la evaporación aumenta el calor latente y disminuye el calor sensible lo cual disminuye la difusión del incendio.",
+      "A través de la convención, al consumir calor latente, provoca un descenso apreciable de la temperatura en la zona afectada.",
+      "El consumo de calor latente durante la radiación tiene como consecuencia una marcada disminución en la temperatura circundante."
+    ]
+  },
+  ];
